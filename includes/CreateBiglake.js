b5d3974239a7abcbe76e07d55ac9be3b6b519602
Shai Harodi <admin@hshai.altostrat.com>
2022-11-21 09:15:49 +0000

    Add comments inline

--- includes/CreateBiglake.js
+++ includes/CreateBiglake.js
@@ -1,5 +1,6 @@
 /*
-* The function ageBucket() takes as input the name of the age field and returns a CASE statement that maps ages age buckets
+The function gets CreateBiglake() takes as inpu two arguments: table name and gcs file path,
+and calls a BQ stored procedure to create a biglake table 
 * You can learn more about functions on https://cloud.google.com/dataform/docs/reuse-code-includes
 */
 
